da998e60c4829fb2b693e5574333854bf96f40c2
antoniemao <antonie.mao@gmail.com>
2025-09-22 21:14:57 -1000

    Navigation Menu (Basic Draft)
    - All pages have a basic nav bar with buttons leading to each page

--- Public/index.js
+++ Public/index.js
@@ -6,3 +6,15 @@ function sendHome() {
 function sendEvents() {
     location.href = "../Events/Events.html";
 }
+
+function sendLessons() {
+    location.href = "../Lessons/Lessons.html";
+}
+
+function sendProjects() {
+    location.href = "../Projects/Projects.html";
+}
+
+function sendSocials() {
+    location.href = "../Socials/Socials.html";
+}
